491cfbacf14c67b311c2ae02a0919e47811d4197
Ilya Zavyalov <ilya.zavyalov0@gmail.com>
2023-11-21 11:17:22 +0300

    Initial commit

---
+++ Scripts/menu.gd
@@ -0,0 +1,10 @@
+extends PanelContainer
+# _ready() and _process(delta)
+
+
+func _on_exit_pressed():
+    get_tree().quit()
+
+
+func _on_start_pressed():
+    get_tree().change_scene_to_file("res://Scenes/test_world.tscn")
